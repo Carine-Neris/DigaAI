367333ba686ae6754a36bb75c15dd149e04dcdbd
Carine <carine_neris@hotmail.com>
2020-03-28 21:26:10 -0300

    add page home e link para cadastro de denuncia

--- DigaAi/core/urls.py
+++ DigaAi/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, cadastro_denuncia
 
 urlpatterns = [
-    path('', home, name='user'),
+    path('', home, name='home'),
+    path('cadastrar/denuncia', cadastro_denuncia, name='cadastro_denuncia'),
 ]
--- DigaAi/core/views.py
+++ DigaAi/core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
+
+
+def cadastro_denuncia(request):
+    return render(request, 'cadastro_denuncia.html')
