e011e0992beb2b6a26de65cea4e8027c39003147
Carine <carine_neris@hotmail.com>
2020-03-28 22:25:30 -0300

    add requirements

--- DigaAi/core/urls.py
+++ DigaAi/core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import home, cadastro_denuncia
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('digaai/', home, name='home'),
     path('cadastrar/denuncia', cadastro_denuncia, name='cadastro_denuncia'),
 ]
