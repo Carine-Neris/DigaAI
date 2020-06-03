1a1aa33e1aa7a713c460d09247ab879abfa51c7f
Carine Neris <carine_neris@hotmail.com>
2020-06-03 19:44:55 -0300

    fix url bug

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import home, cadastro_denuncia
 
 urlpatterns = [
-    path(" ", home, name="home"),
+    path("digaai/", home, name="home"),
     path("cadastrar/denuncia", cadastro_denuncia, name="cadastro_denuncia"),
 ]
 
